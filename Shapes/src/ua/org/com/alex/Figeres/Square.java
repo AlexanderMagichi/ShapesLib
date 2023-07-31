4b23c6220aa51faac3d5df60770a7ea96ce74ba3
Alexander <127439097+AlexanderMagichi@users.noreply.github.com>
2023-07-31 16:07:06 +0300

    added Triangle

--- Shapes/src/ua/org/com/alex/Figeres/Square.java
+++ Shapes/src/ua/org/com/alex/Figeres/Square.java
@@ -4,50 +4,50 @@ import ua.org.com.alex.parentsclasses.SuperShapes;
 
 public class Square extends SuperShapes {
 
-    private int size;
-    private boolean isFilled;
-    private char symbol;
-    private char spaceSymbol;
+	private int size;
+	private boolean isFilled;
+	private char symbol;
+	private char spaceSymbol;
 
-    public Square(String name, int size, char pouring, char symbol, char spaceSymbol, boolean isFilled) {
-        super(name, size, pouring, symbol, spaceSymbol);
-        this.size = size;
-        this.isFilled = isFilled;
-        this.symbol = symbol;
-        this.spaceSymbol = spaceSymbol;
-    
-    }
+	public Square(String name, int size, char pouring, char symbol, char spaceSymbol, boolean isFilled) {
+		super(name, size, pouring, symbol, spaceSymbol);
+		this.size = size;
+		this.isFilled = isFilled;
+		this.symbol = symbol;
+		this.spaceSymbol = spaceSymbol;
 
-    public int getSize() {
-        return size;
-    }
+	}
 
-    public void setSize(int size) {
-        this.size = size;
-    }
+	public int getSize() {
+		return size;
+	}
 
-    public boolean isFilled() {
-        return isFilled;
-    }
+	public void setSize(int size) {
+		this.size = size;
+	}
 
-    public void setFilled(boolean filled) {
-        isFilled = filled;
-    }
+	public boolean isFilled() {
+		return isFilled;
+	}
 
-    public char getSymbol() {
-        return symbol;
-    }
+	public void setFilled(boolean filled) {
+		isFilled = filled;
+	}
 
-    public void setSymbol(char symbol) {
-        this.symbol = symbol;
-    }
+	public char getSymbol() {
+		return symbol;
+	}
 
-    public char getSpaceSymbol() {
-        return spaceSymbol;
-    }
+	public void setSymbol(char symbol) {
+		this.symbol = symbol;
+	}
 
-    public void setSpaceSymbol(char spaceSymbol) {
-        this.spaceSymbol = spaceSymbol;
-    }
+	public char getSpaceSymbol() {
+		return spaceSymbol;
+	}
+
+	public void setSpaceSymbol(char spaceSymbol) {
+		this.spaceSymbol = spaceSymbol;
+	}
 
 }
--- Shapes/src/ua/org/com/alex/parentsclasses/SuperShapes.java
+++ Shapes/src/ua/org/com/alex/parentsclasses/SuperShapes.java
@@ -2,68 +2,67 @@ package ua.org.com.alex.parentsclasses;
 
 public abstract class SuperShapes {
 
-    private String name;
-    private int size;
-    private char pouring;
-    private char spaceSymbol;
-    private char symbol;
+	private String name;
+	private int size;
+	private char pouring;
+	private char spaceSymbol;
+	private char symbol;
 
-    protected SuperShapes(String name, int size, char pouring, char symbol, char spaceSymbol) {
-        this.name = name;
-        this.size = size;
-        this.pouring = pouring;
-        this.symbol = symbol;
-       
-    }
+	protected SuperShapes(String name, int size, char pouring, char symbol, char spaceSymbol) {
+		this.name = name;
+		this.size = size;
+		this.pouring = pouring;
+		this.symbol = symbol;
 
-    public String getName() {
-        return name;
-    }
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getSize() {
-        return size;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setSize(int size) {
-        this.size = size;
-    }
+	public int getSize() {
+		return size;
+	}
 
-    public char getPouring() {
-        return pouring;
-    }
+	public void setSize(int size) {
+		this.size = size;
+	}
 
-    public void setPouring(char pouring) {
-        this.pouring = pouring;
-    }
+	public char getPouring() {
+		return pouring;
+	}
 
-    public char getSymbol() {
-        return symbol;
-    }
+	public void setPouring(char pouring) {
+		this.pouring = pouring;
+	}
 
-    public void setSymbol(char symbol) {
-        this.symbol = symbol;
-    }
+	public char getSymbol() {
+		return symbol;
+	}
 
-    public char getSpaceSymbol() {
-        return spaceSymbol;
-    }
+	public void setSymbol(char symbol) {
+		this.symbol = symbol;
+	}
 
-    public void setSpaceSymbol(char spaceSymbol) {
-        this.spaceSymbol = spaceSymbol;
-    }
+	public char getSpaceSymbol() {
+		return spaceSymbol;
+	}
 
+	public void setSpaceSymbol(char spaceSymbol) {
+		this.spaceSymbol = spaceSymbol;
+	}
 
-    public String toString() {
-        String result = "Тип фигуры: " + name + "\n";
-        result += "Размер: " + size + "\n";
-        result += "Заливка: " + pouring + "\n";
-        result += "Символ заливки: " + symbol + "\n";
-        result += "Пустой символ заливки: " + spaceSymbol + "\n";
+	public String toString() {
+		String result = "Тип фигуры: " + name + "\n";
+		result += "Размер: " + size + "\n";
+		result += "Заливка: " + pouring + "\n";
+		result += "Символ заливки: " + symbol + "\n";
+		result += "Пустой символ заливки: " + spaceSymbol + "\n";
 
-        return result;
-    }
+		return result;
+	}
 }
