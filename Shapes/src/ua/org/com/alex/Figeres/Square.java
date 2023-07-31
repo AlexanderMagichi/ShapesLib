05e75bc49f1705586b82e9d609f4fe0f9e674407
Alexander <127439097+AlexanderMagichi@users.noreply.github.com>
2023-07-31 14:45:15 +0300

    add Auto-generated method in Square

--- Shapes/src/ua/org/com/alex/Figeres/Square.java
+++ Shapes/src/ua/org/com/alex/Figeres/Square.java
@@ -1,5 +1,24 @@
 package ua.org.com.alex.Figeres;
 
-public class Square {
+import ua.org.com.alex.parentsclasses.SuperShapes;
+
+public class Square extends SuperShapes{
+
+	protected Square(String name, int size, char pouring, char fillsymbol) {
+		super(name, size, pouring, fillsymbol);
+		
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculateArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
