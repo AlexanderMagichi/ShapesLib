7f0b2a2559e205169738ad149e9ff3f08a47fb08
Alexander <127439097+AlexanderMagichi@users.noreply.github.com>
2023-08-01 18:50:00 +0300

    why sircle dosent exist?

--- Shapes/src/ua/org/com/alex/libs/CarConstructor.java
+++ Shapes/src/ua/org/com/alex/libs/CarConstructor.java
@@ -10,18 +10,20 @@ import java.util.List;
 import java.util.Random;
 
 public class CarConstructor {
-    public List<Shape> generateRandomCar(){
-        Shape circle = new Circle(5, false, '*', ' ');
-        Shape square = new Square(6, true, 'X');
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(circle);
-        shapes.add(square);
-        List<Shape> result = new ArrayList<>();
-        Random r = new Random();
-        int amount = r.nextInt(4) + 1;
-        for (int i = 0; i < amount; i++) {
-            result.add(r.nextBoolean() ? circle : square);
-        }
-        return result;
-    }
+	public List<Shape> generateRandomCar() {
+		Shape circle = new Circle(5, false, '*', ' ');
+		Shape square = new Square(6, true, 'X');
+		List<Shape> shapes = new ArrayList<>();
+		shapes.add(circle);
+		shapes.add(square);
+		List<Shape> result = new ArrayList<>();
+		Random r = new Random();
+		int amount = r.nextInt(4) + 1;
+		for (int i = 0; i < amount; i++) {
+			result.add(r.nextBoolean() ? circle : square);
+		}
+		return result;
+	}
 }
+
+
